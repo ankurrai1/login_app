435f27c08af989d5c8a519b646b44afb19551d9b
ankur <rai.ankur0007@gmail.com>
2019-05-25 17:09:33 +0530

    bacic get api for app is ready and tested

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const logger = require("morgan"); // to log user requested url on server
+const dotenv = require("dotenv"); // to config envirnment
+const express = require("express");
+const mongoose = require('mongoose');
+const mongo = require("connect-mongo")
+const bodyParser = require("body-parser");
+
+
+const app = express();
+dotenv.config({path: ".env"});
+app.set("port", process.env.PORT);
+
+
+
+app.get('/', function(req, res){ 
+    res.send('<html>all is well...</html>');
+});
+
+
+
+module.exports = app;
---
+++ backend/test/app_test.js
@@ -0,0 +1,34 @@
+const request = require("supertest");
+let assert = require("chai").assert;
+let app = require("../app.js");
+
+describe("APP", () => {
+    beforeEach(function () {
+        handleError = function (done) {
+            return function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                done();
+            };
+        };
+    });
+    describe("GET /bad", () => {
+        it("it should resonds with 404", done => {
+            request(app)
+                .get("/bad")
+                .expect(404)
+                .end(handleError(done));
+        });
+        it("should resonds with 200", done => {
+            request(app)
+                .get("/")
+                .expect(200)
+                .expect("Content-Type", "text/html; charset=utf-8")
+                .expect(/all is well/)
+                .end(handleError(done));
+        });
+    });
+    // describe("GET /", () => {
+    // });
+});
